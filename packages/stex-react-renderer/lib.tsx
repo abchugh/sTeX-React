1609d26da599c24ac5dec6774a782122c230d370
Rakesh Kumar <51470664+XI4507@users.noreply.github.com>
2025-03-06 13:25:54 +0530

    created next and react project for wasm test

--- packages/stex-react-renderer/lib.tsx
+++
@@ -1,4 +0,0 @@
-import { getMMTCustomId } from "@stex-react/utils";
-export function SomeCompo() {
-  return <div>SomeCompo: {getMMTCustomId("SomeCompo")}</div>;
-}
--- packages/utils/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./lib/utils.ts";
-export * from "./lib/file-location.ts";
-export * from "./lib/courseInfo.ts";
-export * from "./lib/resource-action-utils.ts";
