47007c2ebce4e34b965ca8a37160de586cef2abe
Rakesh Kumar <51470664+XI4507@users.noreply.github.com>
2025-03-11 17:14:32 +0530

    ftmldocument and fragment worked in react

--- wasm-react-test/src/App.tsx
+++ wasm-react-test/src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { FTMLDocument, FTMLFragment, setServerUrl } from "@kwarc/ftml-react";
 import "./App.css";
-import { FTMLFragment } from "@kwarc/ftml-react";
 
+setServerUrl("https://mmt.beta.vollki.kwarc.info");
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <FTMLDocument
+        document={{
+          uri: "https://mathhub.info?a=courses/FAU/AI/course&p=course/notes&d=notes1&l=en",
+          toc: "GET",
+        }}
+      />
+      <FTMLFragment
+        fragment={{
+          uri: "https://stexmmt.mathhub.info/:sTeX?a=sTeX/DemoExamples&d=problemtest&l=en&e=exercise_3",
+        }}
+        exercises={(e) => console.log(e)}
+      />
       <FTMLFragment
         fragment={{
           uri: "https://stexmmt.mathhub.info/:sTeX?a=sTeX/DemoExamples&d=problemtest&l=en&e=exercise_1",
