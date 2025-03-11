def13187edb0f819a72e0333ad97cdfb4ea43587
Rakesh Kumar <51470664+XI4507@users.noreply.github.com>
2025-03-11 14:02:22 +0530

    Update App.tsx

--- wasm-react-test/src/App.tsx
+++ wasm-react-test/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { TestDocument, TestFragmentA } from "../../wasm-next-test/src/pages/test";
+import { FTMLFragment } from "@kwarc/ftml-react";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,7 +16,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div> 
+      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -29,8 +29,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <TestDocument />
-      <TestFragmentA />
+      <FTMLFragment
+        fragment={{
+          uri: "https://stexmmt.mathhub.info/:sTeX?a=sTeX/DemoExamples&d=problemtest&l=en&e=exercise_1",
+        }}
+      />
     </>
   );
 }
