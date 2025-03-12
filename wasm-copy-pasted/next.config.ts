a684942cf7b63a9c54f5916df410d0879a3332af
Rakesh Kumar <51470664+XI4507@users.noreply.github.com>
2025-03-12 16:38:16 +0530

    production build fix

--- wasm-copy-pasted/next.config.ts
+++ wasm-copy-pasted/next.config.ts
@@ -1,25 +1,5 @@
 import type { NextConfig } from "next";
-
-function patchWasmModuleImport(config: any, isServer: any) {
-  config.experiments = Object.assign(config.experiments || {}, {
-    asyncWebAssembly: true,
-  });
-
-  config.optimization.moduleIds = "named";
-
-  config.module.rules.push({
-    test: /\.wasm$/,
-    type: "webassembly/async",
-  });
-
-  // TODO: improve this function -> track https://github.com/vercel/next.js/issues/25852
-  if (isServer) {
-    config.output.webassemblyModuleFilename =
-      "./../static/wasm/3daefa5fdc3457b0.wasm";
-  } else {
-    config.output.webassemblyModuleFilename = "static/wasm/3daefa5fdc3457b0.wasm";
-  }
-}
+const CopyPlugin = require("copy-webpack-plugin");
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
@@ -34,7 +14,11 @@ const nextConfig: NextConfig = {
       test: /\.wasm$/,
       type: "webassembly/async",
     });
-    patchWasmModuleImport(config, options.isServer);
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [{ from: "public/wasm", to: "./static/wasm" }],
+      })
+    );
     return config;
   },
 };
