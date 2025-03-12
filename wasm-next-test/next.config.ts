832232852c987dd78a81aaee45fd91b1127260d0
Rakesh Kumar <51470664+XI4507@users.noreply.github.com>
2025-03-12 16:39:04 +0530

    package update

--- wasm-next-test/next.config.ts
+++ wasm-next-test/next.config.ts
@@ -1,8 +1,26 @@
 import type { NextConfig } from "next";
+// const CopyPlugin = require("copy-webpack-plugin");
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.experiments = {
+      asyncWebAssembly: true, //Enable WebAssembly
+      syncWebAssembly: true, //Enable WebAssembly (deprecated)
+      layers: true, // Fixes 'entryOptions.layer' error
+    };
+
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "webassembly/async",
+    });
+    // config.plugins.push(
+    //   new CopyPlugin({
+    //     patterns: [{ from: "public/wasm", to: "./static/wasm" }],
+    //   })
+    // );
+    return config;
+  },
 };
 
 export default nextConfig;
