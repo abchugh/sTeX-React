25a9e3ac9fa146953bc3bd43ebdc2d0dc0660012
Jazzpirate <raupi@jazzpirate.com>
2025-03-12 14:27:47 +0100

    test

--- wasm-copy-pasted/src/pages/test.tsx
+++ wasm-copy-pasted/src/pages/test.tsx
@@ -1,21 +1,21 @@
 import { useEffect } from "react";
 import { FLAMSServer } from "../../utils/flam";
-import { FTMLFragment } from "../../ftml-utils";
+import { FTMLDocument, getFlamsServer } from "../../ftml-utils";
 import { setServerUrl } from "../../ftml-utils";
-const FLAMS_SERVER_URL = "https://mmt.beta.vollki.kwarc.info";
-const server = new FLAMSServer(FLAMS_SERVER_URL);
+
 setServerUrl("https://mmt.beta.vollki.kwarc.info");
 
 const Test = () => {
   useEffect(() => {
-    server.index().then((res) => console.log(res));
+    getFlamsServer().index().then((res) => console.log(res));
   }, []);
 
   return (
     <div>
-      <FTMLFragment
-        fragment={{
-          uri: "https://mathhub.info?a=sTeX/DemoExamples&d=problemtest&l=en&e=exercise_1",
+      <FTMLDocument
+        document={{
+          uri: "https://mathhub.info?a=courses/FAU/AI/course&p=game-play/slides&d=alphago-now&l=en",
+          toc:"GET"
         }}
       />
     </div>
