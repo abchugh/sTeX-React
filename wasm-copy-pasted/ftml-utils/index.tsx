536bd6c959d777e98753f2da013acf8a179a6e55
abchugh <achugh89@gmail.com>
2025-04-23 22:30:54 +0530

    Update index.tsx

--- wasm-copy-pasted/ftml-utils/index.tsx
+++ wasm-copy-pasted/ftml-utils/index.tsx
@@ -36,6 +36,12 @@ export const setDebugLog = FTMLT.setDebugLog;
  * Every attribute is inherited from ancestor nodes *unless explicitly overridden*.
  */
 export interface FTMLConfig {
+
+  /**
+   * whether to allow hovers
+   */
+  allowHovers?: boolean,
+
   /** may return a react component to *insert* after the title of a section
    * @param uri the uri of the section
    * @param lvl the level of the section
@@ -242,6 +248,7 @@ function toConfig(
     onSectionTitle: onSectionTitle,
     onFragment: onFragment,
     problemStates: config.problemStates,
-    onProblem: config.onProblem
+    onProblem: config.onProblem,
+    allowHovers: config.allowHovers,
   };
 }
